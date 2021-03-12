c91148067d529401802971d28ceb3f86fc5e4c9c
alison98 <nosila98@gmail.com>
2021-03-12 11:43:56 -0500

    display colors with rgb values

--- Project4/app/src/main/java/com/example/project4/MainActivity.java
+++ Project4/app/src/main/java/com/example/project4/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.project4;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.project4.MyRecyclerViewAdapter;
+import com.example.project4.R;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements MyRecyclerViewAdapter.ItemClickListener {
+
+    MyRecyclerViewAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // data to populate the RecyclerView with
+        ArrayList<Integer[]> colors = new ArrayList<>();
+        colors.add(new Integer[]{255, 0, 0});
+        colors.add(new Integer[]{255, 255, 0});
+        colors.add(new Integer[]{0, 255, 0});
+        colors.add(new Integer[]{0, 255, 255});
+        colors.add(new Integer[]{0, 0, 255});
+        colors.add(new Integer[]{255, 0, 255});
+
+        // set up the RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.recycler);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new MyRecyclerViewAdapter(this, colors);
+        adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onItemClick(View view, int position) {
+        Toast.makeText(this, "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
     }
 }
---
+++ Project4/app/src/main/java/com/example/project4/MyRecyclerViewAdapter.java
@@ -0,0 +1,81 @@
+package com.example.project4;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAdapter.ViewHolder> {
+
+    private List<Integer[]> mData;
+    private LayoutInflater mInflater;
+    private ItemClickListener mClickListener;
+
+    // data is passed into the constructor
+    public MyRecyclerViewAdapter(Context context, List<Integer[]> data) {
+        this.mInflater = LayoutInflater.from(context);
+        this.mData = data;
+    }
+
+    // inflates the row layout from xml when needed
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.recyclerview_row, parent, false);
+        return new ViewHolder(view);
+    }
+
+    // binds the data to the TextView in each row
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Integer[] rgb = mData.get(position);
+        holder.layout.setBackgroundColor(Color.rgb(rgb[0], rgb[1], rgb[2]));
+        holder.myTextView.setText("" + rgb[0] + ", " + rgb[1] + ", " + rgb[2]);
+    }
+
+    // total number of rows
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+
+    // stores and recycles views as they are scrolled off screen
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView myTextView;
+        LinearLayout layout;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            myTextView = itemView.findViewById(R.id.name);
+            layout = itemView.findViewById(R.id.layout);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (mClickListener != null) mClickListener.onItemClick(view, getAdapterPosition());
+        }
+    }
+
+    // convenience method for getting data at click position
+    Integer[] getItem(int id) {
+        return mData.get(id);
+    }
+
+    // allows clicks events to be caught
+    public void setClickListener(ItemClickListener itemClickListener) {
+        this.mClickListener = itemClickListener;
+    }
+
+    // parent activity will implement this method to respond to click events
+    public interface ItemClickListener {
+        void onItemClick(View view, int position);
+    }
+}
